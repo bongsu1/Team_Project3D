b2e314d500de544dcd8c04d205ccd505127ca129
bongsu <rlaqhdtn22@gmail.com>
2024-03-28 14:43:31 +0900

    feat : IDamagable 인터페이스 구현

---
+++ Assets/Scripts/Interface/IDamagable.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IDamagable
+{
+    /// <summary>
+    /// damage에 받을 데미지 입력
+    /// ex) hp -= damage;
+    /// </summary>
+    /// <param name="damage"></param>
+    public void TakeDamage(int damage);
+}
