f78eaffedf92ed090b55bf595850888eccf1f98a
bongsu <rlaqhdtn22@gmail.com>
2024-04-09 09:35:57 +0900

    docs : 나가기 메뉴 중단

--- Assets/Scripts/Scene/GameScene.cs
+++ Assets/Scripts/Scene/GameScene.cs
@@ -1,62 +1,10 @@
 using System.Collections;
-using TMPro;
-using UnityEngine;
-using UnityEngine.UI;
 
 public class GameScene : BaseScene
 {
-    [Header("HP UI")]
-    [SerializeField] Image[] hpImage;
-
-    [Header("Potion UI")]
-    [SerializeField] TMP_Text potionText;
-    [SerializeField] Image blackPotionImage; // 포션을 다 쓰면 검은색으로 바뀜
-
-    private void OnEnable()
-    {
-        Manager.Game.PlayerData.OnChangeHp += ChangeHPBar;
-        Manager.Game.Inventory.InventoryItem[0].OnChangeItemCount += ChangePotionCount;
-    }
-
-    private void Start()
-    {
-        StartCoroutine(LoadingRoutine()); // test..
-    }
-
-    private void OnDisable()
-    {
-        Manager.Game.PlayerData.OnChangeHp -= ChangeHPBar;
-        Manager.Game.Inventory.InventoryItem[0].OnChangeItemCount -= ChangePotionCount;
-    }
-
-    private void ChangeHPBar(int hp)
-    {
-        for (int i = 0; i < hpImage.Length; i++)
-        {
-            if (i >= hp)
-                hpImage[i].enabled = false;
-            else
-                hpImage[i].enabled = true;
-        }
-    }
-
-    private void ChangePotionCount(int count)
-    {
-        potionText.text = count.ToString();
-        if (count == 0)
-        {
-            blackPotionImage.enabled = true;
-        }
-        else
-        {
-            blackPotionImage.enabled = false;
-        }
-    }
-
     public override IEnumerator LoadingRoutine()
     {
-        ChangeHPBar(Manager.Game.PlayerData.Hp);
-        ChangePotionCount(Manager.Game.Inventory.InventoryItem[0].ItemCount);
+        // 몬스터 스폰 로직 같은 씬로딩때 미리 해야될것들
         yield return null;
     }
 }
