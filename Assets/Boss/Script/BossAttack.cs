876a4f736841c3d32279ec7ede6e83dd11cca28f
RIDDLE <KGA@DESKTOP-9UMGT3Q>
2024-04-15 18:12:24 +0900

    보스 시네마틱 완성

--- Assets/Boss/Script/BossAttack.cs
+++ Assets/Boss/Script/BossAttack.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BossAttack : MonoBehaviour
@@ -7,15 +5,19 @@ public class BossAttack : MonoBehaviour
     [SerializeField] LayerMask playerlayer;
     [SerializeField] int damage;
 
+    HitEffect hitEffect;
+    private void Start()
+    {
+        hitEffect = Instantiate(Manager.Resource.Load<HitEffect>("Effect/HitEffect"), transform);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (((1 << other.gameObject.layer) & playerlayer) != 0)
         {
             IDamagable player = other.GetComponent<IDamagable>();
             player?.TakeDamage(damage);
-            Debug.Log(other.name);
-            Debug.Log(damage);
-            Debug.Log($"플레이어 남은 체력 :{Manager.Game.PlayerData.Hp}");
+            hitEffect.PlayHitEffect(other.ClosestPoint(other.transform.position));
         }
     }
 }
